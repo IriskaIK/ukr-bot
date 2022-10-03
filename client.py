8304b16363c7c6e786d9196cbb91a16dc2b6e4e3
IriskaIK <iris398322@gmail.com>
2022-10-03 09:23:33 +0300

    some fix

--- client.py
+++ client.py
@@ -7,13 +7,14 @@ from googletrans import Translator
 list_of_messages = ['Ти чо сука? на мові балакай підріла. Ще раз побачу таке, дупу порву', 'Ще раз таке напишеш, кікну звідси', 'Вийшов звідси розбійник', 'Sticker']
 async def checkruzzianChar(message:types.Message):
     t = Translator().detect(text = str(message.text))
-    if (t.lang == 'ru' and t.confidence >= 0.6):
-
-        text = choice(list_of_messages)
-        if text == 'Sticker':
-            await bot.send_sticker(message.chat.id, sticker='CAACAgIAAxkBAAEF83hjNUoP6hxP_zwlAqdxahqOjdsnFgACfSQAAgrA-EiyDkpnFi2uqioE' )
-        else:
-            await message.reply(text=text)
+    if (t.lang == 'ru'):
+        index = t.lang.index('ru')
+        if (t.confidence[index] >= 0.9):
+            text = choice(list_of_messages)
+            if text == 'Sticker':
+                await bot.send_sticker(message.chat.id, sticker='CAACAgIAAxkBAAEF83hjNUoP6hxP_zwlAqdxahqOjdsnFgACfSQAAgrA-EiyDkpnFi2uqioE' )
+            else:
+                await message.reply(text=text)
 
     else:
         print(t.lang)
